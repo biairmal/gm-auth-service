f1954f86f524cb81f3a990ded8f91cca8845c138
Bandana Irmal Abdillah <bandana314i@gmail.com>
2023-10-08 09:53:01 +0700

    initial commit

---
+++ src/test/java/id/gudangmeme/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.gudangmeme.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
